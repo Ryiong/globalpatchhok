052b49fd6aff054fbaaa9730095aa0d7306dd0f7
Ryiong <viethung1209.work@gmail.com>
2024-04-01 22:07:53 +0700

    add audio s5

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -25,7 +25,10 @@ musicBtn.addEventListener('click', function () {
         console.log("Pause Music", bgAudiOff)
     }
 })
-
+const audioHoKS5 = new Audio()
+audioHoKS5.autoplay = true;
+audioHoKS5.loop = true;
+audioHoKS5.src = 'assets/music/hok-season5.mp3'
 const openMenu = document.getElementById('open-menu')
 const closeMenu = document.getElementById('close-menu')
 const mobileMenu = document.querySelector('.mobile-down-nav')
@@ -115,4 +118,82 @@ setInterval(function () {
     document.getElementById(d + i).classList.add('black')
     document.getElementById(d + i).classList.remove('noside')
 }, 6000)
+}
+
+// Img Side
+var bigImgSide = document.querySelectorAll('.hp-imgbig');
+var bigTitleSide = document.querySelectorAll('.hp-big');
+var smallImgSide = document.querySelectorAll('.hp-imgsmall');
+var smallTitleSide = document.querySelectorAll('.hp-small');
+
+const bigAPI = [
+    // Hero
+    {
+        name: "Lưu Bị - Nhân Đức Nghĩa Thương",
+        skin: "/2024/03/170-bigskin-1.webp",
+        link: "/p/luubi.html",
+    },
+    // Skin 
+    {
+        name: "Cơ Tiểu Mã - Đại Chiến Quà Vặt",
+        skin: "/2024/03/564-bigskin-3.webp",
+        link: "#",
+    }
+]
+const smallAPI = [
+    // Hero
+    {
+        name: "Hoàng Trung - Pháo Đốt Linh Hồn",
+        skin: "/2024/02/192-mobileskin-1.webp",
+        link: "/p/hoangtrung.html",
+    },
+    {
+        name: "Thượng Quan Uyển Nhi - Kinh Hồng Bút",
+        skin: "/2024/02/513-mobileskin-1.webp",
+        link: "/p/thuongquanuyennhi.html",
+    },
+    {
+        name: "Tào Tháo - Hiếu Hùng Đẫm Máu",
+        skin: "/2024/02/128-mobileskin-1.webp",
+        link: "/p/taothao.html",
+    },
+    {
+        name: "Đao Phong Bảo Bối - Hồ Điệp Sát Thủ",
+        skin: "/2024/03/646-mobileskin-1.webp",
+        link: "/p/daophongbaoboi.html",
+    },
+    // Skin
+    {
+        name: "Charlotte - Phù Sinh Vọng",
+        skin: "/2024/03/536-mobileskin-3.webp",
+        link: "#",
+    },
+    {
+        name: "Mai Shiranui - Phi Nguyệt Hành",
+        skin: "/2024/03/157-mobileskin-3.webp",
+        link: "#",
+    },
+    {
+        name: "Nakoruru - Tiền Trần Kính",
+        skin: "/2024/03/162-mobileskin-3.webp",
+        link: "#",
+    },
+    {
+        name: "Ukyo Tachibana - Phong Sương Tận",
+        skin: "/2024/03/163-mobileskin-3.webp",
+        link: "#",
+    },
+]
+
+for (let i = 0;  i < 2; i++) {
+    bigImgSide[i].src = "https://vgvdpatchglobal.files.wordpress.com" + bigAPI[i].skin;
+    bigImgSide[i].alt = bigAPI[i].name;
+    bigTitleSide[i].href = bigAPI[i].link;
+    bigTitleSide[i].title = bigAPI[i].name;
+}
+for (let i = 0;  i < 8; i++) {
+    smallImgSide[i].src = "https://vgvdpatchglobal.files.wordpress.com" + smallAPI[i].skin;
+    smallImgSide[i].alt = smallAPI[i].name;
+    smallTitleSide[i].href = smallAPI[i].link;
+    smallTitleSide[i].title = smallAPI[i].name;
 }
