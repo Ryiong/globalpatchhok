a23a30e80e3c4b50c15adda0283078c63e33cff8
Ryiong <viethung1209.work@gmail.com>
2024-09-10 22:34:34 +0700

    log js file

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,34 +1,34 @@
 //bgm
-const bgAudio = new Audio()
-bgAudio.autoplay = true;
-bgAudio.loop = true;
-bgAudio.src = 'assets/music/bannerglobal.mp3'
-let bgAudiOff = true
+// const bgAudio = new Audio()
+// bgAudio.autoplay = false;
+// bgAudio.loop = true;
+// bgAudio.src = ''
+// let bgAudiOff = true
 const musicBtn = document.getElementById('btn-music')
 const mspa = document.getElementById('JS-mspa')
 const mpb = document.getElementById('js-music-play')
-bgAudio.pause()
-musicBtn.addEventListener('click', function () {
-    if (bgAudiOff == true) {
-        bgAudio.play()
-        bgAudiOff = false
-        musicBtn.classList.remove("off")
-        musicBtn.classList.add('pm')
-        mspa.classList.remove("hide")
-        console.log("Play Music", bgAudiOff)
-    } else {
-        bgAudio.pause()
-        bgAudiOff = true
-        musicBtn.classList.add("off")
-        musicBtn.classList.remove('pm')
-        mspa.classList.add("hide")
-        console.log("Pause Music", bgAudiOff)
-    }
-})
-const audioHoKS5 = new Audio()
-audioHoKS5.autoplay = true;
-audioHoKS5.loop = true;
-audioHoKS5.src = 'assets/music/hok-season5.mp3'
+// bgAudio.pause()
+// musicBtn.addEventListener('click', function () {
+//     if (bgAudiOff == true) {
+//         bgAudio.play()
+//         bgAudiOff = false
+//         musicBtn.classList.remove("off")
+//         musicBtn.classList.add('pm')
+//         mspa.classList.remove("hide")
+//         console.log("Play Music", bgAudiOff)
+//     } else {
+//         bgAudio.pause()
+//         bgAudiOff = true
+//         musicBtn.classList.add("off")
+//         musicBtn.classList.remove('pm')
+//         mspa.classList.add("hide")
+//         console.log("Pause Music", bgAudiOff)
+//     }
+// })
+// const audioHoKS5 = new Audio()
+// audioHoKS5.autoplay = true;
+// audioHoKS5.loop = true;
+// audioHoKS5.src = 'assets/music/hok-season5.mp3'
 const openMenu = document.getElementById('open-menu')
 const closeMenu = document.getElementById('close-menu')
 const mobileMenu = document.querySelector('.mobile-down-nav')
@@ -41,7 +41,7 @@ function hideMenu() {
     openMenu.classList.remove('hide')
     closeMenu.classList.add('hide')
     mobileMenu.classList.remove('ds')
-
+}
 // Video Banner
 const mainClip = document.getElementById('js-main-clip')
 mainClip.src = 'https://drive.google.com/uc?export=download&id=150_VIrM8FnenNRdFrQbflTmenEMYkvCv'
@@ -62,75 +62,37 @@ var bigTitleSide = document.querySelectorAll('.hp-big');
 var smallImgSide = document.querySelectorAll('.hp-imgsmall');
 var smallTitleSide = document.querySelectorAll('.hp-small');
 
-const bigAPI = [
-    // Hero
-    {
-        name: "Lưu Bị - Nhân Đức Nghĩa Thương",
-        skin: "/2024/03/170-bigskin-1.webp",
-        link: "/p/luubi.html",
-    },
-    // Skin 
-    {
-        name: "Cơ Tiểu Mã - Đại Chiến Quà Vặt",
-        skin: "/2024/03/564-bigskin-3.webp",
-        link: "#",
-    }
-]
-const smallAPI = [
-    // Hero
-    {
-        name: "Hoàng Trung - Pháo Đốt Linh Hồn",
-        skin: "/2024/02/192-mobileskin-1.webp",
-        link: "/p/hoangtrung.html",
-    },
-    {
-        name: "Thượng Quan Uyển Nhi - Kinh Hồng Bút",
-        skin: "/2024/02/513-mobileskin-1.webp",
-        link: "/p/thuongquanuyennhi.html",
-    },
-    {
-        name: "Tào Tháo - Hiếu Hùng Đẫm Máu",
-        skin: "/2024/02/128-mobileskin-1.webp",
-        link: "/p/taothao.html",
-    },
-    {
-        name: "Đao Phong Bảo Bối - Hồ Điệp Sát Thủ",
-        skin: "/2024/03/646-mobileskin-1.webp",
-        link: "/p/daophongbaoboi.html",
-    },
-    // Skin
-    {
-        name: "Charlotte - Phù Sinh Vọng",
-        skin: "/2024/03/536-mobileskin-3.webp",
-        link: "#",
-    },
-    {
-        name: "Mai Shiranui - Phi Nguyệt Hành",
-        skin: "/2024/03/157-mobileskin-3.webp",
-        link: "#",
-    },
-    {
-        name: "Nakoruru - Tiền Trần Kính",
-        skin: "/2024/03/162-mobileskin-3.webp",
-        link: "#",
-    },
-    {
-        name: "Ukyo Tachibana - Phong Sương Tận",
-        skin: "/2024/03/163-mobileskin-3.webp",
-        link: "#",
-    },
-]
+function ConvertDateFormat(date) {
+    let parts = date.split('/');
 
-for (let i = 0;  i < 2; i++) {
-    bigImgSide[i].src = "https://vgvdpatchglobal.files.wordpress.com" + bigAPI[i].skin;
-    bigImgSide[i].alt = bigAPI[i].name;
-    bigTitleSide[i].href = bigAPI[i].link;
-    bigTitleSide[i].title = bigAPI[i].name;
-}
-for (let i = 0;  i < 8; i++) {
-    smallImgSide[i].src = "https://vgvdpatchglobal.files.wordpress.com" + smallAPI[i].skin;
-    smallImgSide[i].alt = smallAPI[i].name;
-    smallTitleSide[i].href = smallAPI[i].link;
-    smallTitleSide[i].title = smallAPI[i].name;
-}
+    let formattedDate = parts[1] + '/' + parts[0] + '/' + parts[2];
 
+    return formattedDate;
+}
+const thumbContentLastest = document.querySelectorAll(".thumb-content-lastest")
+const titleContentLastest = document.querySelectorAll(".title-content-lastest")
+const tagSkinLastest = document.querySelectorAll(".lastest-contentbox i")
+const dateRealTime = ConvertDateFormat((new Date()).toLocaleDateString());
+fetch('https://script.google.com/macros/s/AKfycbz-V3Acodgu-QF-4qYy0Vsp3GfrGKIAUho6SujvtcQMwlLBIS0AV1FVIwG4IVKkukAi/exec')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        thumbContentLastest.forEach((item, index) => {
+            item.src = "https://vgvdpatchglobal.wordpress.com/wp-content/uploads" + data.data[index].bigImg;
+            item.alt = data.data[index].nameSkinVN;
+        })
+        titleContentLastest.forEach((item, index) => {
+            item.innerText = data.data[index].nameHeroVN + "_" + data.data[index].nameSkinVN;
+        })
+        tagSkinLastest.forEach((item, index) => {
+            item.style.background = "url('" + data.data[index].tagSkin + "') 0 0 /100% 100% no-repeat"
+        })
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
