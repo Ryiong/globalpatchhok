5566dcd33e7d0e66fba7fdbcbb53b8f7eda68926
Ryiong <viethung1209.work@gmail.com>
2024-09-19 22:45:24 +0700

    admin template

---
+++ assets/js/dataTemplate.js
@@ -0,0 +1,609 @@
+
+const dataTemplate = [
+    {
+        heroname: "Thiếu Tư Duyên",
+        id: 577,
+        thumb: "./assets/img/TemplatePost/577-poster.png",
+    },
+    {
+        heroname: "Đại Tư Mệnh",
+        id: 517,
+        thumb: "./assets/img/TemplatePost/517-poster.png",
+    },
+    {
+        heroname: "Ngao Ẩn",
+        id: 519,
+        thumb: "./assets/img/TemplatePost/519-poster.png",
+    },
+    {
+        heroname: "Hải Nặc",
+        id: 563,
+        thumb: "./assets/img/TemplatePost/563-poster.png",
+    },
+    {
+        heroname: "Dolia",
+        id: 159,
+        thumb: "./assets/img/TemplatePost/159-poster.png",
+    },
+    {
+        heroname: "Á Liên",
+        id: 514,
+        thumb: "./assets/img/TemplatePost/514-poster.png",
+    },
+    {
+        heroname: "Cơ Tiểu Mãn",
+        id: 564,
+        thumb: "./assets/img/TemplatePost/564-poster.png",
+    },
+    {
+        heroname: "Alessio",
+        id: 545,
+        thumb: "./assets/img/TemplatePost/545-poster.png",
+    },
+    {
+        heroname: "Triệu Hoài Chân",
+        id: 544,
+        thumb: "./assets/img/TemplatePost/544-poster.png",
+    },
+    {
+        heroname: "Hải Nguyệt",
+        id: 521,
+        thumb: "./assets/img/TemplatePost/521-poster.png",
+    },
+    {
+        heroname: "Qua Á",
+        id: 548,
+        thumb: "./assets/img/TemplatePost/548-poster.png",
+    },
+    {
+        heroname: "Tang Khởi",
+        id: 534,
+        thumb: "./assets/img/TemplatePost/534-poster.png",
+    },
+    {
+        heroname: "Phi",
+        id: 542,
+        thumb: "./assets/img/TemplatePost/542-poster.png",
+    },
+    {
+        heroname: "Kim Thiền",
+        id: 540,
+        thumb: "./assets/img/TemplatePost/540-poster.png",
+
+    },
+    {
+        heroname: "Vân Anh",
+        id: 538,
+        thumb: "./assets/img/TemplatePost/538-poster.png",
+    },
+    {
+        heroname: "Ngải Lâm",
+        id: 155,
+        thumb: "./assets/img/TemplatePost/155-poster.png",
+    },
+    {
+        heroname: "Tư Không Chấn",
+        id: 537,
+        thumb: "./assets/img/TemplatePost/537-poster.png",
+
+    },
+    {
+        heroname: "Lan",
+        id: 528,
+        thumb: "./assets/img/TemplatePost/528-poster.png",
+    },
+    {
+        heroname: "Charlotte",
+        id: 536,
+        thumb: "./assets/img/TemplatePost/536-poster.png",
+    },
+    {
+        heroname: "A Cổ Đóa",
+        id: 533,
+        thumb: "./assets/img/TemplatePost/533-poster.png",
+
+    },
+    {
+        heroname: "Mông Điềm",
+        id: 527,
+        thumb: "./assets/img/TemplatePost/527-poster.png",
+    },
+    {
+        heroname: "Kính",
+        id: 531,
+        thumb: "./assets/img/TemplatePost/531-poster.png",
+    },
+    {
+        heroname: "Mông Nha",
+        id: 524,
+        thumb: "./assets/img/TemplatePost/524-poster.png",
+    },
+    {
+        heroname: "Lỗ Ban Đại Sư",
+        id: 525,
+        thumb: "./assets/img/TemplatePost/525-poster.png",
+    },
+    {
+        heroname: "Tây Thi",
+        id: 523,
+        thumb: "./assets/img/TemplatePost/523-poster.png",
+    },
+    {
+        heroname: "Mã Siêu",
+        id: 518,
+        thumb: "./assets/img/TemplatePost/518-poster.png",
+    },
+    {
+        heroname: "Diệu",
+        id: 522,
+        thumb: "./assets/img/TemplatePost/522-poster.png",
+    },
+    {
+        heroname: "Vân Trung Quân",
+        id: 506,
+        thumb: "./assets/img/TemplatePost/506-poster.png",
+    },
+    {
+        heroname: "Yao",
+        id: 505,
+        thumb: "./assets/img/TemplatePost/505-poster.png",
+    },
+    {
+        heroname: "Bàn Cổ",
+        id: 529,
+        thumb: "./assets/img/TemplatePost/529-poster.png",
+    },
+    {
+        heroname: "Trư Bát Giới",
+        id: 511,
+        thumb: "./assets/img/TemplatePost/511-poster.png",
+
+    },
+    {
+        heroname: "Hằng Nga",
+        id: 515,
+        thumb: "./assets/img/TemplatePost/515-poster.png",
+    },
+    {
+        heroname: "Thượng Quan Uyển Nhi",
+        id: 513,
+        thumb: "./assets/img/TemplatePost/513-poster.png",
+    },
+    {
+        heroname: "Lý Tín",
+        id: 507,
+        thumb: "./assets/img/TemplatePost/507-poster.png",
+    },
+    {
+        heroname: "Thẩm Mộng Khê",
+        id: 312,
+        thumb: "./assets/img/TemplatePost/312-poster.png",
+    },
+    {
+        heroname: "Già La",
+        id: 508,
+        thumb: "./assets/img/TemplatePost/508-poster.png",
+    },
+    {
+        heroname: "Thuẫn Sơn",
+        id: 509,
+        thumb: "./assets/img/TemplatePost/509-poster.png",
+    },
+    {
+        heroname: "Tư Mã Ý",
+        id: 137,
+        thumb: "./assets/img/TemplatePost/137-poster.png",
+    },
+    {
+        heroname: "Tôn Sách",
+        id: 510,
+        thumb: "./assets/img/TemplatePost/510-poster.png",
+    },
+    {
+        heroname: "Nguyên Ca",
+        id: 125,
+        thumb: "./assets/img/TemplatePost/125-poster.png",
+    },
+    {
+        heroname: "Mễ Lai",
+        id: 504,
+        thumb: "./assets/img/TemplatePost/504-poster.png",
+    },
+    {
+        heroname: "Cuồng Thiết",
+        id: 503,
+        thumb: "./assets/img/TemplatePost/503-poster.png",
+    },
+    {
+        heroname: "Dịch Tinh",
+        id: 197,
+        thumb: "./assets/img/TemplatePost/197-poster.png",
+    },
+    {
+        heroname: "Bùi Cầm Hổ",
+        id: 502,
+        thumb: "./assets/img/TemplatePost/502-poster.png",
+    },
+    {
+        heroname: "Dương Ngọc Hoàn",
+        id: 176,
+        thumb: "./assets/img/TemplatePost/176-poster.png",
+    },
+    {
+        heroname: "Công Tôn Ly",
+        id: 199,
+        thumb: "./assets/img/TemplatePost/199-poster.png",
+    },
+    {
+        heroname: "Minh Thế Ẩn",
+        id: 501,
+        thumb: "./assets/img/TemplatePost/501-poster.png",
+    },
+    {
+        heroname: "Nữ Oa",
+        id: 179,
+        thumb: "./assets/img/TemplatePost/179-poster.png",
+    },
+    {
+        heroname: "Mộng Kỳ",
+        id: 198,
+        thumb: "./assets/img/TemplatePost/198-poster.png",
+    },
+    {
+        heroname: "Tô Liệt",
+        id: 194,
+        thumb: "./assets/img/TemplatePost/194-poster.png",
+    },
+    {
+        heroname: "Huyền Sách",
+        id: 195,
+        thumb: "./assets/img/TemplatePost/195-poster.png",
+    },
+    {
+        heroname: "Thủ Ước",
+        id: 196,
+        thumb: "./assets/img/TemplatePost/196-poster.png",
+    },
+    {
+        heroname: "Khải",
+        id: 193,
+        thumb: "./assets/img/TemplatePost/193-poster.png",
+    },
+    {
+        heroname: "Quỷ Cốc Tử",
+        id: 189,
+        thumb: "./assets/img/TemplatePost/189-poster.png",
+    },
+    {
+        heroname: "Can Tương Mạc Tà",
+        id: 182,
+        thumb: "./assets/img/TemplatePost/182-poster.png",
+    },
+    {
+        heroname: "Đông Hoàng",
+        id: 187,
+        thumb: "./assets/img/TemplatePost/187-poster.png",
+
+    },
+    {
+        heroname: "Đại Kiều",
+        id: 191,
+        thumb: "./assets/img/TemplatePost/191-poster.png",
+
+    },
+    {
+        heroname: "Hoàng Trung",
+        id: 192,
+        thumb: "./assets/img/TemplatePost/192-poster.png",
+    },
+    {
+        heroname: "Gia Cát Lượng",
+        id: 190,
+        thumb: "./assets/img/TemplatePost/190-poster.png",
+    },
+    {
+        heroname: "Na Tra",
+        id: 180,
+        thumb: "./assets/img/TemplatePost/180-poster.png",
+    },
+    {
+        heroname: "Thái Ất Chân Nhân",
+        id: 186,
+        thumb: "./assets/img/TemplatePost/186-poster.png",
+    },
+    {
+        heroname: "Thái Văn Cơ",
+        id: 184,
+        thumb: "./assets/img/TemplatePost/184-poster.png",
+    },
+    {
+        heroname: "Athena",
+        id: 183,
+        thumb: "./assets/img/TemplatePost/183-poster.png",
+    },
+    {
+        heroname: "Dương Tiễn",
+        id: 178,
+        thumb: "./assets/img/TemplatePost/178-poster.png",
+    },
+    {
+        heroname: "Thành Cát Tư Hãn",
+        id: 177,
+        thumb: "./assets/img/TemplatePost/177-poster.png",
+    },
+    {
+        heroname: "Chung Quỳ",
+        id: 175,
+        thumb: "./assets/img/TemplatePost/175-poster.png",
+    },
+    {
+        heroname: "Ngu Cơ",
+        id: 174,
+        thumb: "./assets/img/TemplatePost/174-poster.png",
+    },
+    {
+        heroname: "Lý Nguyên Phương",
+        id: 173,
+        thumb: "./assets/img/TemplatePost/173-poster.png",
+    },
+    {
+        heroname: "Trương Phi",
+        id: 171,
+        thumb: "./assets/img/TemplatePost/171-poster.png",
+    },
+    {
+        heroname: "Lưu Bị",
+        id: 170,
+        thumb: "./assets/img/TemplatePost/170-poster.png",
+    },
+    {
+        heroname: "Hậu Nghệ",
+        id: 169,
+        thumb: "./assets/img/TemplatePost/169-poster.png",
+    },
+    {
+        heroname: "Ngưu Ma Vương",
+        id: 168,
+        thumb: "./assets/img/TemplatePost/168-poster.png",
+    },
+    {
+        heroname: "Tôn Ngộ Không",
+        id: 167,
+        thumb: "./assets/img/TemplatePost/167-poster.png",
+    },
+    {
+        heroname: "Arthur",
+        id: 166,
+        thumb: "./assets/img/TemplatePost/166-poster.png",
+    },
+    {
+        heroname: "Uyko Tachibana",
+        id: 163,
+        thumb: "./assets/img/TemplatePost/163-poster.png",
+    },
+    {
+        heroname: "Nakoruru",
+        id: 162,
+        thumb: "./assets/img/TemplatePost/162-poster.png",
+    },
+    {
+        heroname: "Mai Shiranui",
+        id: 157,
+        thumb: "./assets/img/TemplatePost/157-poster.png",
+    },
+    {
+        heroname: "Trương Lương",
+        id: 156,
+        thumb: "./assets/img/TemplatePost/156-poster.png",
+    },
+    {
+        heroname: "Hoa Mộc Lan",
+        id: 154,
+        thumb: "./assets/img/TemplatePost/154-poster.png",
+    },
+    {
+        heroname: "Lan Lăng Vương",
+        id: 153,
+        thumb: "./assets/img/TemplatePost/153-poster.png",
+    },
+    {
+        heroname: "Vương Chiêu Quân",
+        id: 152,
+        thumb: "./assets/img/TemplatePost/152-poster.png",
+    },
+    {
+        heroname: "Hàn Tín",
+        id: 150,
+        thumb: "./assets/img/TemplatePost/150-poster.png",
+    },
+    {
+        heroname: "Lưu Bang",
+        id: 149,
+        thumb: "./assets/img/TemplatePost/149-poster.png",
+    },
+    {
+        heroname: "Khương Tử Nha",
+        id: 148,
+        thumb: "./assets/img/TemplatePost/148-poster.png",
+    },
+    {
+        heroname: "Luna",
+        id: 146,
+        thumb: "./assets/img/TemplatePost/146-poster.png",
+    },
+    {
+        heroname: "Trình Giảo Kim",
+        id: 144,
+        thumb: "./assets/img/TemplatePost/144-poster.png",
+    },
+    {
+        heroname: "Angela",
+        id: 142,
+        thumb: "./assets/img/TemplatePost/142-poster.png",
+    },
+    {
+        heroname: "Điêu Thuyền",
+        id: 141,
+        thumb: "./assets/img/TemplatePost/141-poster.png",
+    },
+    {
+        heroname: "Quan Vũ",
+        id: 140,
+        thumb: "./assets/img/TemplatePost/140-poster.png",
+    },
+    {
+        heroname: "Lão Phu Tử",
+        id: 139,
+        thumb: "./assets/img/TemplatePost/139-poster.png",
+    },
+    {
+        heroname: "Võ Tắc Thiên",
+        id: 136,
+        thumb: "./assets/img/TemplatePost/136-poster.png",
+    },
+    {
+        heroname: "Hạng Vũ",
+        id: 135,
+        thumb: "./assets/img/TemplatePost/135-poster.png",
+    },
+    {
+        heroname: "Đạt Ma",
+        id: 134,
+        thumb: "./assets/img/TemplatePost/134-poster.png",
+    },
+    {
+        heroname: "Địch Nhân Kiệt",
+        id: 133,
+        thumb: "./assets/img/TemplatePost/133-poster.png",
+    },
+    {
+        heroname: "Marco Polo",
+        id: 132,
+        thumb: "./assets/img/TemplatePost/132-poster.png",
+    },
+    {
+        heroname: "Lý Bạch",
+        id: 131,
+        thumb: "./assets/img/TemplatePost/131-poster.png",
+    },
+    {
+        heroname: "Musashi",
+        id: 130,
+        thumb: "./assets/img/TemplatePost/130-poster.png",
+    },
+    {
+        heroname: "Điển Vi",
+        id: 129,
+        thumb: "./assets/img/TemplatePost/129-poster.png",
+    },
+    {
+        heroname: "Tào Tháo",
+        id: 128,
+        thumb: "./assets/img/TemplatePost/128-poster.png",
+    },
+    {
+        heroname: "Chân Cơ",
+        id: 127,
+        thumb: "./assets/img/TemplatePost/127-poster.png",
+    },
+    {
+        heroname: "Hạ Hầu Đôn",
+        id: 126,
+        thumb: "./assets/img/TemplatePost/126-poster.png",
+    },
+    {
+        heroname: "Chu Du",
+        id: 124,
+        thumb: "./assets/img/TemplatePost/124-poster.png",
+    },
+    {
+        heroname: "Lữ Bố",
+        id: 123,
+        thumb: "./assets/img/TemplatePost/123-poster.png",
+    },
+    {
+        heroname: "Mị Nguyệt",
+        id: 121,
+        thumb: "./assets/img/TemplatePost/121-poster.png",
+    },
+    {
+        heroname: "Bạch Khởi",
+        id: 120,
+        thumb: "./assets/img/TemplatePost/120-poster.png",
+    },
+    {
+        heroname: "Biển Thước",
+        id: 119,
+        thumb: "./assets/img/TemplatePost/119-poster.png",
+    },
+    {
+        heroname: "Tôn Tẫn",
+        id: 118,
+        thumb: "./assets/img/TemplatePost/118-poster.png",
+    },
+    {
+        heroname: "Chung Vô Diệm",
+        id: 117,
+        thumb: "./assets/img/TemplatePost/117-poster.png",
+    },
+    {
+        heroname: "A Kha",
+        id: 116,
+        thumb: "./assets/img/TemplatePost/116-poster.png",
+    },
+    {
+        heroname: "Cao Tiệm Ly",
+        id: 115,
+        thumb: "./assets/img/TemplatePost/115-poster.png",
+    },
+    {
+        heroname: "Lưu Thiện",
+        id: 114,
+        thumb: "./assets/img/TemplatePost/114-poster.png",
+    },
+    {
+        heroname: "Trang Chu",
+        id: 113,
+        thumb: "./assets/img/TemplatePost/113-poster.png",
+    },
+    {
+        heroname: "Lỗ Ban Đệ Thất",
+        id: 112,
+        thumb: "./assets/img/TemplatePost/112-poster.png",
+    },
+    {
+        heroname: "Tôn Thượng Hương",
+        id: 111,
+        thumb: "./assets/img/TemplatePost/111-poster.png",
+    },
+    {
+        heroname: "Doanh Chính",
+        id: 110,
+        thumb: "./assets/img/TemplatePost/110-poster.png",
+    },
+    {
+        heroname: "Đát Kỷ",
+        id: 109,
+        thumb: "./assets/img/TemplatePost/109-poster.png",
+    },
+    {
+        heroname: "Mặc Tử",
+        id: 108,
+        thumb: "./assets/img/TemplatePost/108-poster.png",
+    },
+    {
+        heroname: "Triệu Vân",
+        id: 107,
+        thumb: "./assets/img/TemplatePost/107-poster.png",
+    },
+    {
+        heroname: "Tiểu Kiều",
+        id: 106,
+        thumb: "./assets/img/TemplatePost/106-poster.png",
+    },
+    {
+        heroname: "Liêm Pha",
+        id: 105,
+        thumb: "./assets/img/TemplatePost/105-poster.png",
+    },
+]
--- assets/js/scale-element.js
+++ assets/js/scale-element.js
@@ -1,9 +1,7 @@
 const elementChange = document.querySelector('.banner-home_content');
-const scaleChange = 0.8943231441048035;
 
 function setScale() {
     if (window.innerWidth < elementChange.clientWidth + 80) {
-        console.log("small")
         let zoom = window.innerWidth / 1024;
         elementChange.style.zoom = zoom
     }
